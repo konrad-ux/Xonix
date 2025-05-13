3b0b04797f90bd7024313154cb4a8e6991644073
Konrad Zawiślak <konradzk20@gmail.com>
2025-05-13 11:10:12 +0200

    adding class teleport and complete enemy.cpp

---
+++ Xonix/Teleport.cpp
@@ -0,0 +1,14 @@
+#include"Teleport.hpp"
+
+
+
+Teleport::Teleport(int x, int y) : m_x(x), m_y(y) {}
+
+
+
+void Teleport::setPosition(int x, int y)
+{
+    m_x = x;
+    m_y = y;
+}
+#include "Teleport.h"
